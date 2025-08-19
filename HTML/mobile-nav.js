e91e54b42a22b43df6d5e413a84c17678aad7be6
titusthuranira468912-max <titusthuranira468912@gmail.com>
2025-08-19 03:21:18 +0300

    Update mobile-nav.js

--- HTML/mobile-nav.js
+++ HTML/mobile-nav.js
@@ -1,1 +1,7 @@
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
 
